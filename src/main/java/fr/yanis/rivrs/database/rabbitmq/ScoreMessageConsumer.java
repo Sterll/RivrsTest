2d18e07a731deb9ea356ee0bc4070a0c50946a59
Yanis - Sterll <yanis.benyacine@ynov.com>
2025-03-29 15:16:18 +0100

    feat: enhance ScoreMessageConsumer to handle score creation and addition

--- src/main/java/fr/yanis/rivrs/database/rabbitmq/ScoreMessageConsumer.java
+++ src/main/java/fr/yanis/rivrs/database/rabbitmq/ScoreMessageConsumer.java
@@ -20,15 +20,30 @@ public class ScoreMessageConsumer extends DefaultConsumer {
         String message = new String(body, StandardCharsets.UTF_8);
 
         String[] parts = message.split(":");
-        if (parts.length == 2) {
-            try {
-                UUID playerUUID = UUID.fromString(parts[0]);
-                int newScore = Integer.parseInt(parts[1]);
+        if (parts.length == 3) {
+            switch (parts[0]){
+                case "create":
+                    try {
+                        UUID playerUUID = UUID.fromString(parts[0]);
+                        int newScore = Integer.parseInt(parts[1]);
 
-                ScoreManager.getScoreManager(playerUUID).setScore(newScore);
-                RMain.getInstance().getLogger().info("Score mis à jour pour " + playerUUID + " : " + newScore);
-            } catch (Exception ex) {
-                ex.printStackTrace();
+                        ScoreManager.getScoreManager(playerUUID).setScore(newScore);
+                        RMain.getInstance().getLogger().info("Score mis à jour pour " + playerUUID + " : " + newScore);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    break;
+                case "add":
+                    try {
+                        UUID playerUUID = UUID.fromString(parts[1]);
+                        int newScore = Integer.parseInt(parts[2]);
+
+                        ScoreManager.getScoreManager(playerUUID).addScore(newScore);
+                        RMain.getInstance().getLogger().info("Score mis à jour pour " + playerUUID + " : " + newScore);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    break;
             }
         }
     }
