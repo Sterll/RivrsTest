57ac7fb5f4c020520191dbbf8a42479b9bd5324d
Yanis - Sterll <yanis.benyacine@ynov.com>
2025-03-29 14:00:09 +0100

    feat: register CommandPos executor for /setpos command in RMain

--- src/main/java/fr/yanis/rivrs/RMain.java
+++ src/main/java/fr/yanis/rivrs/RMain.java
@@ -1,5 +1,6 @@
 package fr.yanis.rivrs;
 
+import fr.yanis.rivrs.commands.CommandPos;
 import fr.yanis.rivrs.event.ZoneListener;
 import fr.yanis.rivrs.manager.ZoneManager;
 import fr.yanis.rivrs.utils.Cuboid;
@@ -7,6 +8,7 @@ import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Map;
+import java.util.Objects;
 
 public final class RMain extends JavaPlugin {
 
@@ -17,6 +19,8 @@ public final class RMain extends JavaPlugin {
         this.loadCuboid();
 
         this.getServer().getPluginManager().registerEvents(new ZoneListener(), this);
+
+        Objects.requireNonNull(this.getCommand("setpos"), "§cLa command '/setpos' n'existe pas").setExecutor(new CommandPos());
     }
 
     @Override
